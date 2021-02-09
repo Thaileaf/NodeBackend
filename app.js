8c9e8e2df052a7e714b955e3d44493f2a43ffd9a
Thaileaf <lethaison2019@gmail.com>
2021-02-08 22:55:57 -0800

    first app js

---
+++ app.js
@@ -0,0 +1,25 @@
+function sayHello(name) {
+    console.log('Hello ' + name)
+}
+
+// sayHello('Moshap')
+console.log(window); // Console is global functions
+
+// Global Objects
+/*
+    setTimeout()
+    clearTimeout()
+
+    setInterval()
+    clearInterval()
+
+    all belong to global object
+    ex: global.console.log()
+
+
+    global.setTimeout()
+
+    variables are local to file
+    var message = ''
+ */
+
